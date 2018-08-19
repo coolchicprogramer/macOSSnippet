83155b5ef6e5480f2f5e1ab2731d7fb964a09990
coolchicprogramer <coolchicprogramer@gmail.com>
2018-08-19 13:53:01 +0900

    Intiial Commit

---
+++ macOSSnippet/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  macOSSnippet
+//
+//  Created by user on 2018/08/19.
+//  Copyright © 2018年 coolchic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
